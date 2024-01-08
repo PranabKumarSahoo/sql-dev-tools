2e9892e33a3105a0763cefd397731641b2a58c6c
kherkatarySan <sandeep21_ug@cse.nits.ac.in>
2024-01-08 23:48:41 +0530

    BUG-Fix: overlapping of drop constraints and count row in diffrent scaling

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -33,28 +33,28 @@ export default function HomePage() {
   return (
     <div className="home-page">
       <h1 className="home-page-title">SQL Tools for Developers</h1>
-      <div id="drop-constraint" className="sections">
-        <h2 className="drop-const-title">Drop Constraint</h2>
+      <div  className="sections">
+        <h2 className="section-title">Drop Constraint</h2>
         <DropConstraintSec />
       </div>
-      <div id="row-count" className="sections">
-        <h2 className="section-2-title">Count of Rows</h2>
+      <div className="sections">
+        <h2 className="section-title">Count of Rows</h2>
         <CountOfRows />
       </div>
-      <div id="select-statement" className="sections">
-        <h2 className="section-3-title">Custom Statement</h2>
+      <div className="sections">
+        <h2 className="section-title">Custom Statement</h2>
         <SelectStatement />
       </div>
-      <div id="nth-highest" className="sections">
-        <h2 className="section-4-title">Nth Highest</h2>
+      <div  className="sections">
+        <h2 className="section-title">Nth Highest</h2>
         <NthHighest />
       </div>
-      <div id="nth-minimum" className="sections">
-        <h2 className="section-5-title">Nth Minimum</h2>
+      <div  className="sections">
+        <h2 className="section-title">Nth Minimum</h2>
         <NthMinimum />
       </div>
-      <div id="section-6" className="sections">
-        <h2 className="section-6-title">Coming Soon...</h2>
+      <div  className="sections">
+        <h2 className="section-title">Coming Soon...</h2>
       </div>
       {showScrollUpButton && (
         <button className='scroll-up-button' onClick={scrollToTop}>
