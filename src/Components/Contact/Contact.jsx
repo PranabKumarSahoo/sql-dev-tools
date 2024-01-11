c4baabc56c2bca126e97301a4e8d8e98b74e751c
Parmesh B <parmeshb90@gmail.com>
2024-01-11 14:09:50 +0530

    updated the contact us page with dark mode

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,78 +3,78 @@ import React from "react";
 export default function Contact() {
   return (
     <>
-      <div class="container my-5 bg-darkblue" >
-        <div class="row justify-content-center">
-          <div class="col-lg-9">
-            <h1 class="mb-3">Contact Us</h1>
+      <div className="container my-5 contact">
+        <div className="row justify-content-center">
+          <div className="col-lg-9">
+            <h1 className="mb-3">Contact Us</h1>
             <form>
-              <div class="row g-3">
-                <div class="col-md-6">
-                  <label for="your-name" class="form-label">
+              <div className="row g-3">
+                <div className="col-md-6">
+                  <label for="your-name" className="form-label">
                     Your Name
                   </label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     id="your-name"
                     name="your-name"
                     required
                   />
                 </div>
-                <div class="col-md-6">
-                  <label for="your-surname" class="form-label">
+                <div className="col-md-6">
+                  <label for="your-surname" className="form-label">
                     Your Surname
                   </label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     id="your-surname"
                     name="your-surname"
                     required
                   />
                 </div>
-                <div class="col-md-6">
-                  <label for="your-email" class="form-label">
+                <div className="col-md-6">
+                  <label for="your-email" className="form-label">
                     Your Email
                   </label>
                   <input
                     type="email"
-                    class="form-control"
+                    className="form-control"
                     id="your-email"
                     name="your-email"
                     required
                   />
                 </div>
-                <div class="col-md-6">
-                  <label for="your-subject" class="form-label">
+                <div className="col-md-6">
+                  <label for="your-subject" className="form-label">
                     Your Subject
                   </label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     id="your-subject"
                     name="your-subject"
                   />
                 </div>
-                <div class="col-12">
-                  <label for="your-message" class="form-label">
+                <div className="col-12">
+                  <label for="your-message" className="form-label">
                     Your Message
                   </label>
                   <textarea
-                    class="form-control"
+                    className="form-control"
                     id="your-message"
                     name="your-message"
                     rows="5"
                     required
                   ></textarea>
                 </div>
-                <div class="col-12">
-                  <div class="row">
-                    <div class="col-md-6">
+                <div className="col-12">
+                  <div className="row">
+                    <div className="col-md-6">
                       <button
                         data-res="<?php echo $sum; ?>"
                         type="submit"
-                        class="btn btn-dark fw-bold w-full"
+                        className="btn btn-dark w-100 fw-bold"
                       >
                         Send
                       </button>
